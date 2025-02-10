829478f7364b7b1b2dede0bd99d521e4f068a5ad
Zulfa Nurfajar <znurfajar@gmail.com>
2025-02-10 22:35:56 +0700

    update

--- etl_scrap.py
+++ etl_scrap.py
@@ -1,117 +1,59 @@
-# import requests
-# from bs4 import BeautifulSoup
-# import pandas as pd
-
-# # URL target untuk web scraping
-# url = "https://sidatagrun-public-1076756628210.asia-southeast2.run.app/ptn_sb.php?ptn=-1"
-
-# # Mengirim permintaan GET untuk mendapatkan HTML
-# response = requests.get(url)
-
-# # Parsing HTML dengan BeautifulSoup
-# soup = BeautifulSoup(response.text, 'html.parser')
-
-# # Mencari tabel yang diinginkan
-# table = soup.find('table', {'class': 'table table-striped'})
-
-# # Menyiapkan list untuk menampung data
-# data = []
-
-# # Mengambil setiap baris dalam tabel
-# rows = table.find_all('tr')[1:]  # Mengabaikan header
-
-# for row in rows:
-#     cols = row.find_all('td')
-    
-#     if len(cols) >= 6:  # Pastikan ada cukup kolom
-#         kode = cols[1].find('a').text.strip()  # Kode di kolom kedua
-#         nama = cols[2].find('a').text.strip()  # Nama di kolom ketiga
-#         kab_kota = cols[3].text.strip()        # Kab/Kota di kolom keempat
-#         provinsi_1 = cols[4].text.strip()      # Provinsi-1 di kolom kelima
-#         provinsi_2 = cols[5].text.strip() if cols[5].text.strip() else None  # Provinsi-2 di kolom keenam (bisa kosong)
-#         link_profil = f"https://sidatagrun-public-1076756628210.asia-southeast2.run.app/ptn_sb.php?ptn={kode[1:]}" # Link profil
-        
-#         # Menambahkan data ke list
-#         data.append({
-#             "kode": kode,
-#             "nama": nama,
-#             "kab_kota": kab_kota,
-#             "provinsi_1": provinsi_1,
-#             "provinsi_2": provinsi_2,
-#             "link_profil": link_profil
-#         })
-
-# # Membuat dataframe dari data yang telah dikumpulkan
-# df = pd.DataFrame(data)
-
-# # Menyimpan dataframe ke CSV
-# csv_file = "coba.csv"
-# df.to_csv(csv_file, index=False)
-
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
+import luigi
 
-# URL utama dengan parameter berbeda (ptn=-1, ptn=-2, ptn=-3)
-url_template = "https://sidatagrun-public-1076756628210.asia-southeast2.run.app/ptn_sb.php?ptn={}"
 
-# Daftar ptn yang akan di-loop
-ptns = [-1, -2, -3]
+# Task untuk mengekstrak data PTN dari SIDATA
+class ExtractPTN(luigi.Task):
+    def requires(self):
+        pass # Tidak ada task yang diperlukan
+    
+    def output(self):
+        return luigi.LocalTarget('raw-data/raw_list_univ.csv') # Tempat penyimpanan data yang diekstrak
 
-# Fungsi untuk melakukan web scraping dan menyimpan hasil ke CSV
-def scrape_and_save(ptn_value):
-    # Menghasilkan URL dengan ptn yang berbeda
-    url = url_template.format(ptn_value)
+    def run(self):
+        # URL untuk mengakses data PTN di SIDATA
+        base_url = "https://sidatagrun-public-1076756628210.asia-southeast2.run.app/ptn_sb.php?ptn={}"
+        # URL utama dengan parameter berbeda (ptn=-1, ptn=-2, ptn=-3)
+        pages = [-1, -2, -3]
+        # List untuk menyimpan data universitas
+        list_universitas = []
 
-    # Mengirim permintaan GET untuk mendapatkan HTML
-    response = requests.get(url)
+        for page in pages:
+            url = base_url.format(page) # Membuat URL dengan parameter page
+            response = requests.get(url) # Mengirim permintaan GET ke URL
+            soup = BeautifulSoup(response.text, 'html.parser') # Parsing HTML dengan BeautifulSoup
+            table = soup.find('table', {'class': 'table table-striped'}) # Mencari tabel dengan class 'table table-striped'
+            rows = table.find_all('tr')[1:] # Mengambil semua baris dalam tabel, kecuali header
 
-    # Parsing HTML dengan BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
+            for row in rows: # Iterasi setiap baris dalam tabel
+                cols = row.find_all('td')
 
-    # Mencari tabel yang diinginkan
-    table = soup.find('table', {'class': 'table table-striped'})
+                if len(cols) >= 6: # Pastikan jumlah kolom cukup
+                    kode = cols[1].find('a').text.strip() # Mengambil kode universitas
+                    nama = cols[2].find('a').text.strip() # Mengambil nama universitas
+                    kab_kota = cols[3].text.strip() # Mengambil kab/kota universitas
+                    provinsi_1 = cols[4].text.strip() # Mengambil provinsi-1 universitas
+                    provinsi_2 = cols[5].text.strip() if cols[5].text.strip() else None # Mengambil provinsi-2 universitas (bisa kosong)
+                    link_profil = f'https://sidatagrun-public-1076756628210.asia-southeast2.run.app/ptn_sb.php?ptn={kode[1:]}' # Link profil universitas
 
-    # Menyiapkan list untuk menampung data
-    data = []
+                    # Menambahkan data universitas ke dalam list
+                    list_universitas.append({
+                        'kode': kode,
+                        'nama': nama,
+                        'kab_kota': kab_kota,
+                        'provinsi_1': provinsi_1,
+                        'provinsi_2': provinsi_2,
+                        'link_profil': link_profil
+                    })
+        raw_univ_df = pd.DataFrame(list_universitas) # Membuat DataFrame dari list universitas
+        raw_univ_df.to_csv(self.output().path, index=False) # Menyimpan DataFrame ke dalam file CSV
 
-    # Mengambil setiap baris dalam tabel
-    rows = table.find_all('tr')[1:]  # Mengabaikan header
 
-    for row in rows:
-        cols = row.find_all('td')
-        
-        if len(cols) >= 6:  # Pastikan ada cukup kolom
-            kode = cols[1].find('a').text.strip()  # Kode di kolom kedua
-            nama = cols[2].find('a').text.strip()  # Nama di kolom ketiga
-            kab_kota = cols[3].text.strip()        # Kab/Kota di kolom keempat
-            provinsi_1 = cols[4].text.strip()      # Provinsi-1 di kolom kelima
-            provinsi_2 = cols[5].text.strip() if cols[5].text.strip() else None  # Provinsi-2 di kolom keenam (bisa kosong)
-            link_profil = f"https://sidatagrun-public-1076756628210.asia-southeast2.run.app/ptn_sb.php?ptn={kode[1:]}"  # Link profil
 
-            # Menambahkan data ke list
-            data.append({
-                "kode": kode,
-                "nama": nama,
-                "kab_kota": kab_kota,
-                "provinsi_1": provinsi_1,
-                "provinsi_2": provinsi_2,
-                "link_profil": link_profil
-            })
-
-    # Membuat dataframe dari data yang telah dikumpulkan
-    df = pd.DataFrame(data)
-
-    # Menyimpan dataframe ke CSV
-    csv_file = f"data_{ptn_value}.csv"
-    df.to_csv(csv_file, index=False)
-
-    # Mengembalikan path file CSV
-    return csv_file
-
-# Iterasi untuk setiap ptn dan simpan hasilnya
-csv_files = [scrape_and_save(ptn) for ptn in ptns]
-
-# Menampilkan file CSV yang telah disimpan
-csv_files
-
+# Memulai eksekusi program
+if __name__ == '__main__':
+    # Membangun pipeline Luigi dengan urutan tugas yang telah ditentukan
+    luigi.build([ExtractPTN()],
+                local_scheduler=True) # Menjalankan scheduler lokal untuk mengatur eskusi tugas
